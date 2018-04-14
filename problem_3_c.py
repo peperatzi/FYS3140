f0395b2fff5b42c5e3e6a0104d61e7b1c8d42ee1
Hans-Petter Harveg <hanspetter@spillerom.no>
2018-04-14 12:15:08 +0900

    Added figures for the coding problem.

--- problem_3_c.py
+++ problem_3_c.py
@@ -3,35 +3,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-#f(1) if a <= 1 <= b else 0
-
 def phi(x, n):
     if np.abs(x) >= 1./n:
         return 0
     else:
         return n/2.
 
+def f_1(x, a):
+    return (x-a)*(x+a)
+
+
 # 
 N = 101
-x = np.linspace(0, 2*np.pi, N)
+x = np.linspace(-2*np.pi, 2*np.pi, N)
 y = np.zeros(N)
-y1 = np.zeros(N)
+f = np.sin(x)
+#a = 3
+#f = (x-a)*(x+a)
 for i in range(N):
-    for t in range(1, 100):
-        for n in range(1, t):
-            test = np.sin(x[i]-(2./np.pi))
-            y[i] = phi(test, n)
-            y1[i] = np.sin(x[i])
+    for n in range(1, 100):
+        #y[i] += phi(np.sin(x[i]), n)
+        y[i] += phi(f[i]), n)/2500.
 
+plt.plot(x,y, x, f,"--")
 
-#y1 = np.sin(x)
-
-# Plot the crazyness
 plt.xlabel('x')
-plt.ylabel('y')
-plt.plot(x, y, 'b', x, y1, 'g')
-#plt.plot(x, y, 'b')
+plt.ylabel('$\Phi(f(x))$')
+plt.legend(['$\Phi(x)$','$f(x)$'])
 plt.show()
 
 
